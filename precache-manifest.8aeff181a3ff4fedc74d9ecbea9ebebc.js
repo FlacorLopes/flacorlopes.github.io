b627fdb27c10e45d92e340c0c975b172e3e23ee9
Flacor Lopes <flaviovinicius95@hotmail.com>
2020-09-26 12:02:25 -0300

    commit inicial

---
+++ precache-manifest.8aeff181a3ff4fedc74d9ecbea9ebebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1473499873b3d9fc1a14de1b2174eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa40e157a9702d60a946",
+    "url": "/static/css/main.7ff633f6.chunk.css"
+  },
+  {
+    "revision": "e7708fedac0f4e1a22f2",
+    "url": "/static/js/2.d7d63e95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7d63e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa40e157a9702d60a946",
+    "url": "/static/js/main.33cb62f1.chunk.js"
+  },
+  {
+    "revision": "e286e0b3dbf3952a9c67",
+    "url": "/static/js/runtime-main.5f55a65a.js"
+  },
+  {
+    "revision": "1a373e873a1be36747f881badea93660",
+    "url": "/static/media/poster.1a373e87.PNG"
+  }
+]);
